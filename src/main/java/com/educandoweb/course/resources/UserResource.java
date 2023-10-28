ddf5006a45143be88e415b28b36723287759d18b
Eric Oliveira <oiericoi@hotmail.com>
2023-10-27 22:27:19 -0300

    feat: add user resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1, "Eric", "eric@email.com", "9987117231", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}

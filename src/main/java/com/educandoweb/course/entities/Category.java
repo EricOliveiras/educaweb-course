19a0e59a8c7bdade624a29524fefa6d353432908
Eric Oliveira <oiericoi@hotmail.com>
2023-10-30 14:40:04 -0300

    refactor: add products field in category class

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,9 +1,12 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -12,6 +15,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private final Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -33,8 +39,8 @@ public class Category implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
